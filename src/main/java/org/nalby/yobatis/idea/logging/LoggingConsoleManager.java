d0a787cd53b244ea27666f024bc11d17402ec0f0
Santiago <linbaiye@gmail.com>
2018-11-11 16:58:25 +0800

    fix tool window sharing issue. (#2)

--- src/main/java/org/nalby/yobatis/idea/logging/LoggingConsoleManager.java
+++ src/main/java/org/nalby/yobatis/idea/logging/LoggingConsoleManager.java
@@ -4,6 +4,7 @@ import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowManager;
@@ -15,32 +16,48 @@ public class LoggingConsoleManager {
 
     private ConsoleView consoleView;
 
-    private Content content;
-
     private ToolWindow toolWindow;
 
-    public LoggingConsoleManager(ConsoleView consoleView, Content content, ToolWindow toolWindow) {
+    private Content content;
+
+    private final static Key<ConsoleView> CONSOLE_KEY = new Key<>("consoleView");
+    private final static Key<ToolWindow> WINDOW_KEY = new Key<>("toolWindow");
+    private final static Key<Content> CONTENT_KEY = new Key<>("content");
+
+    public LoggingConsoleManager(ConsoleView consoleView, ToolWindow toolWindow, Content content) {
         this.consoleView = consoleView;
-        this.content = content;
         this.toolWindow = toolWindow;
+        this.content = content;
     }
 
-    public void activateLoggingConsole() {
-        toolWindow.activate(() -> {}, true, true);
-        toolWindow.getContentManager().setSelectedContent(content);
+    public void activateLoggingConsole(Project project) {
+        toolWindow = project.getUserData(WINDOW_KEY);
+        content = project.getUserData(CONTENT_KEY);
+        consoleView = project.getUserData(CONSOLE_KEY);
+        if (toolWindow != null && content != null) {
+            toolWindow.activate(() -> { }, true, true);
+            toolWindow.getContentManager().setSelectedContent(content);
+        }
     }
 
     public void appendError(String msg) {
-        consoleView.print(msg, ConsoleViewContentType.ERROR_OUTPUT);
+        if (consoleView != null) {
+            consoleView.print(msg, ConsoleViewContentType.ERROR_OUTPUT);
+        }
     }
 
     public void appendDebug(String msg) {
-        consoleView.print(msg, ConsoleViewContentType.NORMAL_OUTPUT);
+        if (consoleView != null) {
+            consoleView.print(msg, ConsoleViewContentType.NORMAL_OUTPUT);
+        }
     }
 
     public void appendInfo(String msg) {
-        consoleView.print(msg, ConsoleViewContentType.NORMAL_OUTPUT);
+        if (consoleView != null) {
+            consoleView.print(msg, ConsoleViewContentType.NORMAL_OUTPUT);
+        }
     }
+
 
     public synchronized static LoggingConsoleManager getInstance() {
         if (instance == null) {
@@ -49,7 +66,9 @@ public class LoggingConsoleManager {
         return instance;
     }
 
-    public synchronized static LoggingConsoleManager init(Project project) {
+
+    public synchronized static LoggingConsoleManager newInstance(Project project) {
+
         ToolWindow outputWindow = ToolWindowManager.getInstance(project).registerToolWindow("Yobatis Output",
                 true, ToolWindowAnchor.BOTTOM);
 
@@ -58,7 +77,10 @@ public class LoggingConsoleManager {
         Content content = outputWindow.getContentManager().getFactory().createContent(consoleView.getComponent(),
                 "", false);
         outputWindow.getContentManager().addContent(content);
-        instance = new LoggingConsoleManager(consoleView, content, outputWindow);
+        project.putUserData(WINDOW_KEY, outputWindow);
+        project.putUserData(CONSOLE_KEY, consoleView);
+        project.putUserData(CONTENT_KEY, content);
+        instance = new LoggingConsoleManager(consoleView, outputWindow, content);
         return instance;
     }
 }
---
+++ src/main/java/org/nalby/yobatis/idea/ui/YobatisToolWindowFactory.java
@@ -0,0 +1,27 @@
+package org.nalby.yobatis.idea.ui;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+import org.nalby.yobatis.core.log.LoggerFactory;
+import org.nalby.yobatis.idea.logging.IdeaLogger;
+import org.nalby.yobatis.idea.logging.LoggingConsoleManager;
+
+
+public class YobatisToolWindowFactory implements ToolWindowFactory {
+
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+
+        LoggingAwareCommandExecutor executor = LoggingAwareCommandExecutor.newInstance(project);
+        YobatisToolWindow yobatisToolWindow = new YobatisToolWindow(toolWindow, executor);
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(yobatisToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
+        yobatisToolWindow.executeLoadAll();
+    }
+}
